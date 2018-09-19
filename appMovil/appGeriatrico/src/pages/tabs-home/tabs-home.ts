cacc594c6d64cd6ed54e35d8100953df878f48d5
Cristian Franco <cristianfranco@Cristians-MacBook-Pro.local>
2018-09-19 09:58:54 -0500

    Se agregan 3 segmentos a la vista de gestion. (Clientes, personal, inventario)

--- appMovil/appGeriatrico/src/pages/tabs-home/tabs-home.ts
+++ appMovil/appGeriatrico/src/pages/tabs-home/tabs-home.ts
@@ -1,5 +1,8 @@
+import { NotificacionesPage } from './../notificaciones/notificaciones';
+import { GestionPage } from './../gestion/gestion';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CalendarioPage } from '../calendario/calendario';
 
 /**
  * Generated class for the TabsHomePage page.
@@ -14,7 +17,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tabs-home.html',
 })
 export class TabsHomePage {
-
+  tab1Root = CalendarioPage;
+  tab2Root = GestionPage;
+  tabAbout = NotificacionesPage;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
