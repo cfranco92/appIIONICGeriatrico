a132e1dc044c1f67457dfabc3a380929881ce3b4
Cristian Franco <cristianfranco@Cristians-MacBook-Pro.local>
2018-10-02 23:22:28 -0500

    Guarda foto en storage y trae url

--- appMovil/appGeriatrico/src/pages/crear-cliente/crear-cliente.ts
+++ appMovil/appGeriatrico/src/pages/crear-cliente/crear-cliente.ts
@@ -1,5 +1,5 @@
 import { Camera, CameraOptions } from '@ionic-native/camera';
-import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { AngularFireStorage, AngularFireUploadTask, AngularFireStorageReference } from 'angularfire2/storage';
 import { Observable } from 'rxjs/Observable';
 import { finalize } from 'rxjs/operators';
 import { AcudientePage } from './../acudiente/acudiente';
@@ -21,18 +21,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CrearClientePage {
   cliente: any = {};  
+  ref: AngularFireStorageReference;
   task: AngularFireUploadTask;
+  uploadProgress: Observable<number>;
+  downloadURL: Observable<string>;
 
   progress: any;  // Observable 0 to 100
-
   image: string; // base64
+  url: Observable<string>;
   constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera, private storage: AngularFireStorage) {
       this.cliente = navParams.get('cliente') || {};
       this.cliente.acudiente= {};
       this.cliente.parientes={pariente1: {},pariente2: {}}
       this.cliente.medico = {};
       this.cliente.seguros = {};
-      this.cliente.observaciones = {};      
+      this.cliente.observaciones = {};             
   }
 
   async takePhoto() {
@@ -47,58 +50,42 @@ export class CrearClientePage {
     return await this.camera.getPicture(options)
   }
 
-  createUploadTask(file: string): void {
-    const filePath = `my-pet-crocodile_${ new Date().getTime() }.jpg`;
-    this.image = 'data:image/jpg;base64,' + file;
-    this.task = this.storage.ref(filePath).putString(this.image, 'data_url');
+  // createUploadTask(file: string): void {
+  //   const filePath = `usuarios/usuario_${ new Date().getTime() }.jpg`;
+  //   this.image = 'data:image/jpg;base64,' + file;
+  //   this.task = this.storage.ref(filePath).putString(this.image, 'data_url');
 
-    this.progress = this.task.percentageChanges();
+  //   this.progress = this.task.percentageChanges();
+  // }
+
+  createUploadTask(file: string) {
+    const filePath = `usuarios/usuario_${ new Date().getTime() }.jpg`;
+    this.ref = this.storage.ref(filePath);
+    this.image = 'data:image/jpg;base64,' + file;
+    alert("Cargando foto en la base de datos");
+    const task = this.ref.putString(this.image, 'data_url').then(() => {      
+      this.ref.getDownloadURL().subscribe(url => {        
+        this.downloadURL = url;
+        this.cliente.foto = this.downloadURL;
+        alert("Foto cargada con éxito en la base de datos")
+      })
+    });
+    // this.uploadProgress = this.task.percentageChanges();   
+     
   }
 
   async uploadHandler() {
    const base64 = await this.takePhoto();
-   this.createUploadTask(base64);
+   this.createUploadTask(base64);   
   }
   
-  
-  
-  // takePhoto(){
-  //   const options: CameraOptions = {
-  //     quality: 100,
-  //     // targetHeight:,
-  //     // targetWidth:,
-  //     destinationType: this.camera.DestinationType.FILE_URI,
-  //     encodingType: this.camera.EncodingType.JPEG,
-  //     mediaType: this.camera.MediaType.PICTURE
-  //   }
-  //   this.camera.getPicture(options).then((imageData) => {
-  //     // imageData is either a base64 encoded string or a file URI
-  //     // If it's base64 (DATA_URL):
-  //     let file = 'data:image/jpeg;base64,' + imageData;      
-
-  //     // const file = imageData.target.files[0];
-  //     const filePath = 'usuarios' + this.cliente.nombre;
-  //     const fileRef = this.storage.ref(filePath);
-  //     const task = this.storage.upload(filePath, file);
-      
-  //     // get notified when the download URL is available
-  //     task.snapshotChanges().pipe(
-  //       finalize(() => {
-  //         this.downloadURL = fileRef.getDownloadURL();
-  //         this.cliente.foto = fileRef.getDownloadURL();
-  //       })
-  //     )
-  //     .subscribe()
-  //    }, (err) => {
-  //     // Handle error
-  //    });
-  // }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CrearClientePage');
   }
 
   irAVistaAcudiente(cliente){
+    this.cliente.foto = this.downloadURL;
     this.navCtrl.push(AcudientePage, {cliente: cliente});
   }
 
